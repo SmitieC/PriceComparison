b2408c21ce6eec8cd2312627441180ca62622094
SmitieC <83625219+SmitieC@users.noreply.github.com>
2022-05-05 10:11:28 +1200

    Set up

---
+++ Com2_Instructions_v1.py
@@ -0,0 +1,5 @@
+""" Component 2 of Price Checker Program
+Ask user if used program before if so continue to program,
+if not display instructions
+5/05/2022 Conor Smith"""
+
---
+++ PC-Base.py
@@ -0,0 +1,24 @@
+"""Base Component of Price Checker program
+Include all components into one program
+Conor Smith"""
+
+# Import Statements
+
+# Functions go here
+
+# Main
+
+# set up dictionary's and lists
+
+# ask user if they have used the program before
+# show instructions if they would like
+
+    # loop to get item info
+
+    # Get name (cannot be blank)
+
+    # Get budget (cannot be blank, must be a number)
+
+    # Calculate best value
+
+# Print results to user

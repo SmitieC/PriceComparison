ad26e731e51079ade56bc2c81f1d06e4f648e85e
SmitieC <83625219+SmitieC@users.noreply.github.com>
2022-05-19 10:28:37 +1200

    Component 2

--- Com2_Instructions_v2.py
+++ Com2_Instructions_v2.py
@@ -1,7 +1,7 @@
 """ Component 2 of Price Checker Program
 Ask user if used program before if so continue to program,
 if not display instructions
-V1 is to create function to check for yes/no responses
+V2 receive response from function. continue if yes and print instructions if no
 17/05/2022 Conor Smith"""
 
 
@@ -17,5 +17,15 @@ def yes_no_checker(question):
 
 
 # Main routine
+instructions = "***Instructions***\n" \
+               "This program will compare and produce the best value items\n" \
+               "based on the following criteria:\n" \
+               "1. Price\n" \
+               "2. Quantity\n" \
+               "3. Budget\n" \
+               "The program will then display the best value item\n" \
+               "Thank you for using this program!"
 display_instructions = yes_no_checker("Have you used this program before?"
                                       "\n(yes/no)\n")
+if display_instructions is False:
+    print(instructions)
--- PC-Base.py
+++ PC-Base.py
@@ -2,9 +2,44 @@
 Include all components into one program
 Conor Smith"""
 
+
 # Import Statements
 
 # Functions go here
+# Blank checker function
+def not_blank(question):
+    valid = ""
+    # using .isalpha check for response
+    while not valid.isalpha():
+        valid = input(question)
+        if valid == "" or valid.isalpha() != True:
+            print("This cannot be blank...")
+        else:
+            return valid
+
+
+# Number checker function
+def num_check(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
+
+
+# float checker function
+def float_check(question):
+    while True:
+        try:
+            float_num = input(question)
+            return float(float_num)
+
+        except ValueError:
+            print("please enter valid number:\n")
+
 
 # Main
 
@@ -13,12 +48,12 @@ Conor Smith"""
 # ask user if they have used the program before
 # show instructions if they would like
 
-    # loop to get item info
+# Get name (cannot be blank)
+name = not_blank("What is your name: ")
+# Get budget (cannot be blank, must be a number)
+budget = float_check("What is your budget?\n$")
+# loop to get item info
 
-    # Get name (cannot be blank)
-
-    # Get budget (cannot be blank, must be a number)
-
-    # Calculate best value
+# Calculate best value
 
 # Print results to user
